247f51fa750f56eedaa5e554d1fa223c9c74eca5
Gabriel Coronado <gcoronado5100@gmail.com>
2023-08-31 11:25:02 -0600

    Build: Funcionan primeros intentos de migraciones

---
+++ database/migrations/2023_08_31_170355_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique()->nullable();
+            $table->string('phone')->unique();
+            $table->string('address_street');
+            $table->string('address_ext');
+            $table->string('address_int');
+            $table->string('address_col');
+            $table->string('address_town');
+            $table->string('address_state');
+            $table->string('address_zip');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
